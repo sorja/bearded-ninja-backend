fe989145c25b9eb408d52d5165d0948e82ee8746
Miró Sorja <miro.sorja@digital-identity.com>
2014-05-08 13:53:21 +0300

    Added rack-cors

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,11 @@ module BeardedNinjaBe
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  end
+        config.middleware.use Rack::Cors do
+          allow do
+            origins '*'
+            resource '*', :headers => :any, :methods => [:get, :post, :options]
+          end
+        end
+    end
 end
