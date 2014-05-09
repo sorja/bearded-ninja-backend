2a516d7b7b623898ed5c292fa2523638542a7952
Niemi Joe J <joeniemi@da1-cs-b221-06.helsinki.fi>
2014-05-09 17:10:18 +0300

    test

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -81,5 +81,6 @@ class ImagesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
      # params.require(:image).permit(:name, :imageUrl, :views)
+      params.require(:image).permit(:file)
     end
 end
