189e88bee7a14f69ec7b5314f093c53e2c784d08
Joe Niemi <niemijoe@hotmail.com>
2014-05-10 14:41:07 +0300

    test

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,7 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :verify_authenticity_token
+  
   # GET /images
   # GET /images.json
   def index
