d8af66700a3698ac4445c5e3a31148fe7043c998
Joe Niemi <niemijoe@hotmail.com>
2014-05-10 14:43:21 +0300

    test

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,7 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
   skip_before_action :verify_authenticity_token
-  
+
   # GET /images
   # GET /images.json
   def index
@@ -38,6 +38,7 @@ class ImagesController < ApplicationController
     ind = ind + 1
     end
     @image.imageUrl = @url
+    
 
     respond_to do |format|
       if @image.save
@@ -82,6 +83,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:name, :imageUrl, :views)
+      params.require(:image).permit(:file)
     end
 end
